63804eb79b7a229e54f2fada8299054708c5454e
Jared Hale <60709856+060664-swcsd@users.noreply.github.com>
2020-02-05 13:15:19 -0500

    Work

---
+++ gameTools.js
@@ -0,0 +1,23 @@
+exports.id = 'gameTools';
+
+class Player {
+	constructor() {
+		// prease
+	}
+}
+
+class Game {}
+
+class GameHandler {
+	constructor() {
+		this.Games = [];
+	}
+
+	getGameById(id) {
+		for (var game in this.Games) {
+			break;
+		}
+	}
+
+	getGameByName(name) {}
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
-const express = require("express"),
-  path = require("path"),
-  fs = require("fs"),
-  sio = require("socket.io"),
-  http = require("http"),
-  utils = require("./util.js");
+const express = require('express'),
+	path = require('path'),
+	fs = require('fs'),
+	sio = require('socket.io'),
+	http = require('http'),
+	utils = require('./util.js');
 
 const port = 8080;
 
 let app = express(),
-  httpServ = http.createServer(app),
-  io = sio(httpServ);
+	httpServ = http.createServer(app),
+	io = sio(httpServ);
 
-app.use(express.static(__dirname + "/html"));
-app.use(express.static(__dirname + "/html/js"));
+app.use(express.static(__dirname + '/html'));
+app.use(express.static(__dirname + '/html/js'));
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/html/index.html");
+// Guanine
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/html/index.html');
 });
 
-io.on("connection", socket => {
-  console.log("User connected!");
+io.on('connection', socket => {
+	console.log('User connected! Hooking into their socket now.');
 
-  socket.theirUUID = utils.generateID(16);
-  socket.theirNickname = "UNO Player " + Math.floor(Math.random() * 1000000);
+	socket.theirUUID = utils.generateID(16);
+	socket.theirNickname = 'Player ' + Math.floor(Math.random() * 1000000);
 
-  console.log(socket.theirNickname);
+	socket.on('joinHandshake', gameId => {
+		socket.requestedGame = gameId;
+		socket.currentGame = gameId;
+	});
+	socket.on('gameReadyUp', () => {
+		if (!socket.currentGame) {
+			// TODO: Kill the socket, it's evil!!!
+			return;
+		}
+	});
 });
 
 httpServ.listen(port, function() {
-  console.log("Listening on port " + port.toString());
+	console.log('Listening on port ' + port.toString());
 });
